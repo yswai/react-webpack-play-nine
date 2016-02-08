9b93e35df84b16f93e9c6e6aa7890a46a5e4d2f6
ysw <yswai1986@gmail.com>
2016-02-08 20:21:25 +0800

    reduced number of tries

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ class AppComponent extends React.Component {
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
         <hr/>
-        <Game size="9" maxRetries="50" />
+        <Game size="9" maxRetries="10" />
         <hr/>
         <MyCounter counter/>
         <hr/>
